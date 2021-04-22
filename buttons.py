f0aa6bdf2deba7034131d3c043858be30315025b
emmaetoemma <75483946+emmaetoemma@users.noreply.github.com>
2021-04-22 23:17:50 +0300

    Create buttons.py

---
+++ buttons.py
@@ -0,0 +1,135 @@
+step_0_buttons = [
+    {
+        'title': 'Да',
+        'hide': True
+    },
+    {
+        'title': 'Нет',
+        'hide': True
+    }
+]
+
+
+step_1_buttons = [
+    {
+        'title': 'Пляжная вечеринка',
+        'hide': True
+    },
+    {
+        'title': 'Ужин вдвоём',
+        'hide': True
+    },
+    {
+        'title': 'Торжественный гала ужин',
+        'hide': True
+    },
+    {
+        'title': 'Диджей пати в клубе',
+        'hide': True
+    }
+]
+
+# second step doesn't contain any buttons
+
+step_3_buttons = [
+            {
+                'title': 'Грязные танцы',
+                'hide': True
+            },
+            {
+                'title': 'Давайте потанцуем',
+                'hide': True
+            },
+            {
+                'title': 'Запах женщины',
+                'hide': True
+            },
+            {
+                'title': 'Супер Майкл иксиксэль',
+                'hide': True
+            }
+        ]
+
+step_4_buttons = [
+            {
+                'title': '1',
+                'hide': True
+            },
+            {
+                'title': '2',
+                'hide': True
+            },
+            {
+                'title': '3',
+                'hide': True
+            },
+            {
+                'title': '4',
+                'hide': True
+            },
+            {
+                'title': '5',
+                'hide': True
+            }
+        ]
+
+step_5_buttons = [
+            {
+                'title': 'Медленный вальс',
+                'hide': True
+            },
+            {
+                'title': 'Аргентинское танго',
+                'hide': True
+            },
+            {
+                'title': 'Сальса',
+                'hide': True
+            }
+        ]
+
+step_6_buttons = [
+            {
+                'title': 'Фиолетовый',
+                'hide': True
+            },
+            {
+                'title': 'Красный',
+                'hide': True
+            },
+            {
+                'title': 'Синий',
+                'hide': True
+            },
+            {
+                'title': 'Оранжевый',
+                'hide': True
+            },
+            {
+                'title': 'Зелёный',
+                'hide': True
+            },
+            {
+                'title': 'Жёлтый',
+                'hide': True
+            },
+            {
+                'title': 'Пудровый',
+                'hide': True
+            },
+            {
+                'title': 'Бирюзовый',
+                'hide': True
+            }
+        ]
+
+step_7_buttons = [
+    {
+        'title': 'Да',
+        'hide': True
+    },
+    {
+        'title': 'Нет',
+        'hide': True
+    }
+]
